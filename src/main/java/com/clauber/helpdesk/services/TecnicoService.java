2bb1401d0a8b2ed41943f0f8cdb36faed4fe7f93
Clauber Martins Pinto <clauber.pinto@mapa.magov.intranet>
2022-01-17 17:04:46 -0300

    criando delete para técnico implementado

--- src/main/java/com/clauber/helpdesk/services/TecnicoService.java
+++ src/main/java/com/clauber/helpdesk/services/TecnicoService.java
@@ -48,6 +48,15 @@ public class TecnicoService {
 		return repository.save(oldObj);
 	}
 
+	public void delete(Integer id) {
+		Tecnico obj = findById(id);
+
+		if (obj.getChamados().size() > 0) {
+			throw new DataIntegrityViolationException("O técnico possui ordens de serviço e não pode ser deletado!");
+		}
+		repository.deleteById(id);
+	}
+
 	private void validaPorCpfEEmail(TecnicoDTO objDTO) {
 		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
 		if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
